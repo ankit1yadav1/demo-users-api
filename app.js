f7fdcf12b65b9d4b538dcd351091a75cbf4188e7
ankit1yadav1 <47358223+ankit1yadav1@users.noreply.github.com>
2020-09-06 00:43:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+app.use(bodyParser.json()); // application/json
+
+app.use(userRoutes);
+
+app.listen(8080);
